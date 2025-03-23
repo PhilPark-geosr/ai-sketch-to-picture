e45cf01ac6b6e917403cd78810b5f2a90c355b6e
Youn <tfcpa0716@gmail.com>
2025-03-23 19:33:27 +0900

    feat(dev): add tanstack query structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,14 @@ import './index.css'
 import Router from '@/routes/index.tsx'
 import { Provider } from 'react-redux'
 import store from '@/store/index.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <Router />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  </QueryClientProvider>
 )
