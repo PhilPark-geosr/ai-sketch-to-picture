4026a5823967afa2fe680872f315894d48008f81
Youn <tfcpa0716@gmail.com>
2025-04-21 14:10:41 +0900

    feat(dev): add testcase of uploadedImageUrl

--- frontend/src/test/DrawingCanvas.test.tsx
+++ frontend/src/test/DrawingCanvas.test.tsx
@@ -3,7 +3,8 @@ import { render, screen, fireEvent } from '@testing-library/react'
 import { vi } from 'vitest'
 import DrawingCanvas from '@/components/DrawingCanvas'
 import defaultImage from '@/assets/chair.png'
-import '@testing-library/jest-dom'
+import successImage from '@/assets/sidebar-icon.png'
+import '@testing-library/jest-dom' // ToBeIndocument()등의 메소드를 쓰려면 필요
 // 모킹할 훅 import
 import * as sketchHook from '../hooks/sketch'
 
@@ -23,5 +24,31 @@ describe('DrawingCanvas', () => {
 
     render(<DrawingCanvas />)
     expect(screen.getByText('idle')).toBeInTheDocument()
-  })
+  }),
+    it('test', async () => {
+      // 모듈 전체를 모킹하여 테스트
+      // mockUseCreateSketch.useCreateSketch.mockReturnValue 로 쓰는 이유
+      // -> useCreateSketch자체는 동기적 함수이기 떄문!
+      // 그안에 잇는 mutateAsync function은 비동기 함수이기 떄문에 resolve써야 함
+
+      vi.mock('../hooks/sketch')
+      const mockUseCreateSketch = vi.mocked(sketchHook)
+      mockUseCreateSketch.useCreateSketch.mockReturnValue({
+        result: {
+          data: null,
+          status: 'success',
+          mutateAsync: vi.fn().mockResolvedValue({
+            image: defaultImage
+          })
+        },
+        uploadedImageUrl: successImage
+      })
+      render(<DrawingCanvas />)
+
+      //상태 잘 넘어오는지 확인
+      expect(screen.getByText('success')).toBeInTheDocument()
+      const elem = screen.getByTestId('uploadImageUrl')
+      // uploadedImageUrl이 정확하게 넘어오고 있는지 확인
+      expect(elem).toHaveTextContent(successImage)
+    })
 })
