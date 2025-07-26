9986616562fcfb7a1e6573721123d0003e23a965
park224.phil <park224.phil@samsung.com>
2025-07-26 18:33:36 +0900

    refactor(recommend): imageCard 컴포넌트 추가

--- frontend/src/routes/pages/Recommendation.tsx
+++ frontend/src/routes/pages/Recommendation.tsx
@@ -1,6 +1,6 @@
-import defaultImage from '@/assets/chair.png'
 import { mockRecommendData } from '@/mock/recommend_mock.ts'
 import { recommendResponse, recommendResult } from '../../mock/recommend_mock'
+import ImageCard from '../../components/ImageCard'
 export default function Recommendation() {
   const responseData: recommendResponse = mockRecommendData
   const result: recommendResult[] = responseData.results
@@ -11,30 +11,10 @@ export default function Recommendation() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {responseData.status === 'success' &&
           result.map((item, idx) => (
-            <div
+            <ImageCard
               key={idx}
-              className="bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-lg transition-shadow p-4">
-              <img
-                src={item.image}
-                alt="추천 이미지"
-                className="w-full h-48 object-cover rounded-lg mb-4"
-              />
-              <p className="text-gray-700 mb-2">
-                <span className="font-semibold">주소: </span>
-                <a
-                  href={item.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-500 underline">
-                  링크 열기
-                </a>
-              </p>
-              {item.price && (
-                <p className="text-gray-800 font-semibold">
-                  가격: {item.price}
-                </p>
-              )}
-            </div>
+              item={item}
+            />
           ))}
       </div>
     </div>
