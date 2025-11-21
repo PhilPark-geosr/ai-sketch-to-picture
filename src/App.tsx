7f7846084c4863d50e6b5d13ef30f34faccf7678
Youn <tfcpa0716@gmail.com>
2025-11-21 18:05:39 +0900

    feat(router): router basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,48 @@ import GallerySelectView from './pages/GallerySelectView'
 import { MemoSketch } from './components/MemoSketch'
 import TodoList from './components/TodoList'
 import RecommendView from './components/RecommendView'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native'
+
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function MemoSketchWrapper() {
+  return (
+    <View style={styles.container}>
+      <MemoSketch uploadUrl={'set your ip'} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator initialRouteName="MemoSketchView">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="MemoSketchView" component={MemoSketchWrapper} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <View>
-        <Text style={styles.dummyText}>Hello world!</Text>
-      </View> */}
-      {/* <GallerySelectView></GallerySelectView> */}
-      {/* <MemoSketch uploadUrl={'set your ip'}></MemoSketch> */}
-      <RecommendView />
-      {/* <TodoList></TodoList> */}
-    </View>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   )
 }
 
 const styles = StyleSheet.create({
   safe: { flex: 1, backgroundColor: '#ffffff' },
-  container: { flex: 1, padding: 16, gap: 16 },
+  container: { flex: 1, padding: 16, gap: 16, backgroundColor: '#ffffff'},
   title: { fontSize: 20, fontWeight: '700' },
   subtitle: { color: '#6b7280' },
   dummyText: {
